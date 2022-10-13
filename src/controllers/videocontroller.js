0c8415cff5298830ef2fd85abb4b03a3b4861e04
sanghyeok1000 <parksang10000@naver.com>
2022-10-13 20:37:05 +0900

    delete things

--- src/controllers/videocontroller.js
+++ src/controllers/videocontroller.js
@@ -36,7 +36,10 @@ export const watch = (req, res) => {
   const video = videos[id - 1];
   return res.render("watch", { pageTitle: `watching ${video.title}`, video });
 };
-export const edit = (req, res) => res.render("edit");
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("upload");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("edit", { pageTitle: `Editing : ${video.title}`, video });
+};
+
+export const postEdit = (req, res) => {};
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -9,6 +9,5 @@ const handleHome = (req, res) => res.send("Home");
 globalRouter.get("/", trending);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
-globalRouter.get("/search", search);
 
 export default globalRouter;
