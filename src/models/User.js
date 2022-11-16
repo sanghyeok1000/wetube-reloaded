531f046e2164f266af97e76ccf41939253759502
sanghyeok1000 <parksang10000@naver.com>
2022-11-16 19:17:43 +0900

    hashing password

--- src/models/User.js
+++ src/models/User.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
@@ -6,6 +7,10 @@ const userSchema = new mongoose.Schema({
   password: { type: String, reequired: true },
   name: { type: String, reequired: true },
   location: String,
+});
+
+userSchema.pre("save", async function () {
+  this.password = await bcrypt.hash(this.password, 5);
 });
 
 const User = mongoose.model("user", userSchema);
