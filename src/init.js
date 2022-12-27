fa0bd93135be0aa58c89c08d8d48a4945ce87d36
sanghyeok1000 <parksang10000@naver.com>
2022-12-27 14:26:47 +0900

    restart

--- src/init.js
+++ src/init.js
@@ -6,6 +6,6 @@ import app from "./server";
 const PORT = 4000;
 
 const handleListening = () =>
-  console.log(`server listening on port http://localhost:${PORT}`);
+  console.log(`✅server listening on port http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
