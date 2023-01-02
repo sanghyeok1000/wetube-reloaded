21e2966e526dc9848d5ca6c1bec6fbe5394f2604
sanghyeok1000 <parksang10000@naver.com>
2023-01-02 21:29:47 +0900

    github login

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,6 @@
 export const localMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
-  res.locals.loggedIn = req.session.user;
+  res.locals.loggedInUser = req.session.user;
   next();
 };
--- src/models/User.js
+++ src/models/User.js
@@ -3,9 +3,10 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
+  socialOnly: { type: Boolean, default: false },
   username: { type: String, required: true, unique: true },
-  password: { type: String, reequired: true },
-  name: { type: String, reequired: true },
+  password: { type: String },
+  name: { type: String, required: true },
   location: String,
 });
 
