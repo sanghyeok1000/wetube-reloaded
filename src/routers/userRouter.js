dfeb2d46ef35b6b0c6faa0d0a6a8a728c7de7ef3
sanghyeok1000 <parksang10000@naver.com>
2023-01-03 18:13:51 +0900

    completed user authentication

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,7 +1,6 @@
 import express from "express";
 import {
   edit,
-  remove,
   logout,
   see,
   startGithubLogin,
@@ -14,7 +13,6 @@ const handleEdit = (req, res) => res.send("Edit user");
 
 userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
-userRouter.get("/remove", remove);
 userRouter.get("/github/start", startGithubLogin);
 userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get(":id", see);
